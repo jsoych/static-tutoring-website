df1474c02346d245ee91ac0153f82d53d202ada2
Soych <jsoychak@uwaterloo.ca>
2022-02-08 21:07:54 -0500

    changes to the cool chalk board file size and polishing expertise and bio

---
+++ welcome_get.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email <?php echo $_GET["email"]; ?><br>
+Your message <?php echo $_GET["message"]; ?>
+
+</body>
+</html>
